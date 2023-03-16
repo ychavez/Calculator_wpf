4e442001d54c35f59ec00fe101f79d2a5b750dd2
Yael Chavez <you400@gmail.com>
2023-03-15 18:14:36 -0600

    observableColection

--- Productos_wpf/ViewModel/ProductsViewModel.cs
+++ Productos_wpf/ViewModel/ProductsViewModel.cs
@@ -1,32 +1,63 @@
 ﻿using Productos_wpf.DataContext;
 using Productos_wpf.Models;
 using Productos_wpf.ViewModel.Services;
+using Productos_wpf.Views;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Windows;
 using System.Windows.Input;
 
 namespace Productos_wpf.ViewModel
 {
-    public class ProductsViewModel
+    public class ProductsViewModel : INotifyPropertyChanged
     {
+        private readonly ProductsContext context;
+        private readonly NewWindow newWindow;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public ICommand NewProductCommand { get; set; }
 
-        public List<Product> productList { get; set; }
-
-
-        public ProductsViewModel(ProductsContext context)
+        private ObservableCollection<Product> _productList;
+        public ObservableCollection<Product> productList
         {
-            productList = context.Products.ToList();
-            NewProductCommand =
-                new CommandHandler(NuevoProducto, () => true);
+            get { return _productList; }
+            set
+            {
+                _productList = value;
+                ExecPropertyChanged(nameof(productList));
+            }
         }
 
 
-        void NuevoProducto() 
+        public ProductsViewModel(ProductsContext context, NewWindow newWindow)
         {
-            MessageBox.Show("Nuevo producto");
+            productList = new ObservableCollection<Product>(context.Products.ToList());
+
+            NewProductCommand = new CommandHandler(NuevoProducto, EvaluarProductos);
+            this.context = context;
+            this.newWindow = newWindow;
+        }
+
+
+        bool EvaluarProductos()
+        {
+            return productList.Count() <= 15;
+        }
+
+        void NuevoProducto()
+        {
+            newWindow.ShowDialog();
+            productList = new ObservableCollection<Product>(context.Products.ToList());
+        }
+
+        void ExecPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged is not null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
